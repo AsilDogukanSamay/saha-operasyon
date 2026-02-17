a716814a2d814641b4e9ddbe86a3ebd6761e406b
AsilDogukanSamay <121396434+AsilDogukanSamay@users.noreply.github.com>
2026-02-17 17:09:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -369,7 +369,7 @@ st.markdown("""
     
     /* Sidebar Logo HD Ayarı */
     .hd-sidebar-logo {
-        width: 100%;
+        width: 70%;
         border-radius: 15px;
         image-rendering: -webkit-optimize-contrast;
         box-shadow: 0 4px 15px rgba(0,0,0,0.4);
